1989f49fc9921473fbaba07195e5c4cb815f1c87
Ben deLong <bendelong45@gmail.com>
2018-10-04 23:57:19 -0500

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,108 +1,171 @@
-// initialize variables and functions for game
+$(document).ready(function () {
+  // initialize variables and functions for game
+  $("#timer-holder").hide();
+  $(".question-answer").hide();
+  $(".right-wrong").hide();
+  var timerID;
+  var questionIndex;
+  var userGuess;
+  var answersRight;
+  var questions = [{
+    question: "Who is the regional manager of the Dunder Mifflin Scranton branch?",
+    answers: ["Dwight Schrute", "Michael Scott", "Pam Beesly", "Meredith Palmer",],
+    correctAnswer: "Michael Scott",
+  },
+  {
+    question: "Who does Michael impersonate that leads to the branch having a Diversity Day?",
+    answers: ["Eddie Murphy", "Nicki Minaj", "Robin Williams", "Chris Rock"],
+    correctAnswer: "Chris Rock",
+  },
 
-$("#timer-holder").hide();
-$(".question-answer").hide();
-$("#right-wrong").hide();
-var timerID;
-var questionIndex;
-var userGuess;
-var answersRight = 0;
-var questions = [{
-  question: "Test question 1",
-  answers: ["testq1 1", "testq1 2", "testq1 3", "testq1 4"],
-  correctAnswer: "testq1 2",
-},
-{
-  question: "Test question 2",
-  answers: ["testq2 1", "testq2 2", "testq2 3", "testq2 4"],
-  correctAnswer: "testq2 4",
+  {
+    question: "Who's the only character that shares their name with the exact name of the actor?",
+    answers: ["Jim Halpert", "Pam Beesly", "Meredith Palmer", "Creed Bratton"],
+    correctAnswer: "Creed Bratton",
+  },
 
-}
-];
+  {
+    question: "What is Roy, from the warehouse,'s last name?",
+    answers: ["Anderson", "Phillips", "Grotti", "Martinez"],
+    correctAnswer: "Anderson",
+  },
 
-function startGame() {
-  questionIndex = 0;
-  $("#timer").text(timer)
-  $(".question-answer").show();
-  $("#timer-holder").show();
-  $("#start-button").hide();
-  setQuestion();
-};
+  {
+    question: "What movie does the office watch on Movie Monday?",
+    answers: ["Speed", "Varsity Blues", "The Mask", "A League of Their Own"],
+    correctAnswer: "Varsity Blues",
+  },
 
-function startTimer() {
-  $("#timer-holder").show();
-  var timer = 15;
-  $("#timer").text(timer);
-  timerID = setInterval(function () {
-    timer--;
-    $("#timer").text(timer);
-    if (timer === 0) {
-      clearInterval(timerID);
-      $(".question-answer").hide();
-      $("#right-wrong-image").attr("src", "assets/images/Time_Out_Gif.gif");
-      $("#right-wrong-message").html("TIME'S UP!<br>The Correct Answer is: " + questions[questionIndex].correctAnswer);
-      $("#right-wrong").show();
-      questionIndex++;
-      setTimeout(setQuestion, 7000);
-    };
-  }, 1000);
-}
+  {
+    question: "What is \"Dinkin Flicka\"?",
+    answers: ["A game people play in the office when bored", "Dunder Mifflin's biggest client", "A made up \"Black People\" saying Darrell teaches Michael", "A tasty Scranton delicacy"],
+    correctAnswer: "A made up \"Black People\" saying Darrell teaches Michael",
+  },
 
-function setQuestion() {
-  if (questionIndex < questions.length) {
-    $(".question-answer").show();
-    $("#question").text(questions[questionIndex].question);
-    $("#answer1").text(questions[questionIndex].answers[0]);
-    $("#answer2").text(questions[questionIndex].answers[1]);
-    $("#answer3").text(questions[questionIndex].answers[2]);
-    $("#answer4").text(questions[questionIndex].answers[3]);
-    startTimer();
-    $("#right-wrong").hide();
+  {
+    question: "What does Michael try to order at Hooters, much to the waitress' disgust?",
+    answers: ["Milk", "A lap dance", "Chicken breast, hold the chicken", "A Big Mac"],
+    correctAnswer: "Chicken breast, hold the chicken",
+  },
+
+  {
+    question: "What is the name of the security guard at the Scranton branch?",
+    answers: ["Hank", "Lou", "Chief", "Garth"],
+    correctAnswer: "Hank",
+  },
+
+  {
+    question: "Instead of hiring a stripper, who does Jim hire for Phyllis' bachelorette party?",
+    answers: ["Spongebob Squarepants", "Spider-Man", "Benjamin Franklin", "A singing dog"],
+    correctAnswer: "Benjamin Franklin",
+  },
+
+  {
+    question: "What type of property does Dwight own, where he operates a Bed and Breakfast?",
+    answers: ["Scranton's tallest building", "A parking lot with a one bedroom shack", "The basement of the office building", "A 16-acre beet farm with farmhouse"],
+    correctAnswer: "A 16-acre beet farm with farmhouse",
   }
-  else {
-    $("#start-button").show();
-    $("#right-wrong-message").text("You got " + answersRight + "/" + questions.length + " questions correct!");
-    if (answersRight < 2) {
-      $("#right-wrong-image").attr("src", "assets/images/Loss_Gif.gif");
+  ];
+
+  function startGame() {
+    questionIndex = 0;
+    answersRight = 0;
+    $("#timer").text(timer)
+    $(".question-answer").show();
+    $("#timer-holder").show();
+    $("#start-button").hide();
+    setQuestion();
+  };
+
+  function startTimer() {
+    $("#timer-holder").show();
+    var timer = 15;
+    $("#timer").text(timer);
+    timerID = setInterval(function () {
+      timer--;
+      $("#timer").text(timer);
+      if (timer === 0) {
+        clearInterval(timerID);
+        $(".question-answer").hide();
+        $("#right-wrong-image").attr("src", "assets/images/Time_Out_Gif.gif");
+        $("#right-wrong-message").html("TIME'S UP!<br>The Correct Answer is: " + questions[questionIndex].correctAnswer);
+        $(".right-wrong").show();
+        questionIndex++;
+        setTimeout(setQuestion, 7000);
+      };
+    }, 1000);
+  }
+
+  function setQuestion() {
+    if (questionIndex < questions.length) {
+      $(".question-answer").show();
+      $("#question").text(questions[questionIndex].question);
+      $("#answer1").text(questions[questionIndex].answers[0]);
+      $("#answer2").text(questions[questionIndex].answers[1]);
+      $("#answer3").text(questions[questionIndex].answers[2]);
+      $("#answer4").text(questions[questionIndex].answers[3]);
+      startTimer();
+      $(".right-wrong").hide();
     }
     else {
-      $("#right-wrong-image").attr("src", "assets/images/Win_Gif.gif");
+      $("#start-button").show();
+      $("#right-wrong-message").text("You got " + answersRight + "/" + questions.length + " questions correct!");
+      if (answersRight === 0) {
+        $("#right-wrong-image").attr("src", "assets/images/I_am_dead.gif");
+      }
+      else if (answersRight > 0 && answersRight < 3) {
+        $("#right-wrong-image").attr("src", "assets/images/stanley_im_done.gif");
+      }
+      else if (answersRight >= 3 && answersRight < 5) {
+        $("#right-wrong-image").attr("src", "assets/images/creed_gif.gif");
+      }
+      else if (answersRight >= 5 && answersRight < 7) {
+        $("#right-wrong-image").attr("src", "assets/images/dwight_clapping.gif");
+      }
+      else if (answersRight >= 7 && answersRight < 10) {
+        $("#right-wrong-image").attr("src", "assets/images/yes_yes_yesyes.gif");
+      }
+      else if (answersRight === 10) {
+        $("#right-wrong-image").attr("src", "assets/images/andy_nailed_it.gif");
+      }
+      $("#timer-holder").hide();
     }
-    $("#timer-holder").hide();
-  }
-};
+  };
 
-$("#start-button").on("click", function () {
-  startGame();
+  function checkAnswer() {
+    if (userGuess === questions[questionIndex].correctAnswer) {
+      $("#right-wrong-image").attr("src", "assets/images/Win_Gif.gif");
+      $("#right-wrong-message").text("Raise the roof for that right answer!");
+      $(".question-answer").hide();
+      $("#timer-holder").hide();
+      $(".right-wrong").show();
+      answersRight++;
+      questionIndex++;
+      setTimeout(setQuestion, 5000);
+    }
+    else {
+      $("#right-wrong-image").attr("src", "assets/images/Loss_Gif.gif");
+      $("#right-wrong-message").html("Well you really biffed it on that one...<br>The correct answer is:<br>" + questions[questionIndex].correctAnswer);
+      $(".question-answer").hide();
+      $("#timer-holder").hide();
+      $(".right-wrong").show();
+      questionIndex++;
+      setTimeout(setQuestion, 5000);
+    }
+  }
+
+  $("#start-button").on("click", function () {
+    startGame();
+  });
+
+  $(".answer-button").on("click", function () {
+    clearInterval(timerID);
+    userGuess = $(this).text();
+    checkAnswer();
+  });
+
 });
 
-$(".answer-button").on("click", function () {
-  clearInterval(timerID);
-  userGuess = $(this).text();
-  checkAnswer();
-});
-
-function checkAnswer() {
-  if (userGuess === questions[questionIndex].correctAnswer) {
-    $("#right-wrong-image").attr("src", "assets/images/Win_Gif.gif");
-    $("#right-wrong-message").text("CORRECT!");
-    $(".question-answer").hide();
-    $("#timer-holder").hide();
-    $("#right-wrong").show();
-    answersRight++;
-    questionIndex++;
-    setTimeout(setQuestion, 7000);
-  }
-  else {
-    $("#right-wrong-image").attr("src", "assets/images/Loss_Gif.gif");
-    $("#right-wrong-message").html("WRONG!<br>The Correct Answer is: " + questions[questionIndex].correctAnswer);
-    $(".question-answer").hide();
-    $("#timer-holder").hide();
-    $("#right-wrong").show();
-    questionIndex++;
-    setTimeout(setQuestion, 7000);
-  }
-}
 
 
 
